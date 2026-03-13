bf27835fe2ab2b1d4975d76d0261b8ce93ae6599
VirajShah97 <virajshah19977@gmail.com>
2026-03-12 21:38:16 -0400

    fix: correctly resolve environment variables

--- ingestion/wmata_ingestion.py
+++ ingestion/wmata_ingestion.py
@@ -5,7 +5,7 @@ import requests
 import psycopg2
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 logging.basicConfig(
     level=logging.INFO,
