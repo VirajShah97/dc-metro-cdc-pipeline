80ddeab7951fbb7e90481cf8086ebb39c84eb072
VirajShah97 <virajshah19977@gmail.com>
2026-04-05 21:03:04 -0400

    feat: add new endpoint table to postgres

--- infrastructure/postgres_setup.sql
+++ infrastructure/postgres_setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE train_predictions (
+CREATE TABLE IF NOT EXISTS train_predictions (
     id SERIAL PRIMARY KEY,
     station_code VARCHAR(10),
     station_name VARCHAR(100),
@@ -12,3 +12,18 @@ CREATE TABLE train_predictions (
 );
 
 ALTER TABLE train_predictions REPLICA IDENTITY FULL;
+
+CREATE TABLE IF NOT EXISTS train_positions (
+    id                       SERIAL PRIMARY KEY,
+    train_id                 VARCHAR(10),
+    car_count                INTEGER,
+    direction_num            INTEGER,
+    circuit_id               INTEGER,
+    destination_station_code VARCHAR(10),
+    line_code                VARCHAR(5),
+    seconds_at_location      INTEGER,
+    service_type             VARCHAR(20),
+    ingested_at              TIMESTAMP DEFAULT NOW()
+);
+
+ALTER TABLE train_positions REPLICA IDENTITY FULL;
